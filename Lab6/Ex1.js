9e7b2cbd8b72e0afb1daf27428b4779f4faa28c3
pnoh <pnoh@hawaii.edu>
2020-09-22 11:50:00 -1000

    lab6

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+age = 54;
+fav_num = 5;
+day_of_birth = 12;
+month_of_birth = 4;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+console.log("\n");
+console.log((age + fav_num) / day_of_birth * month_of_birth);
---
+++ Lab6/Ex5.js
@@ -0,0 +1,6 @@
+first_name = 'Philip';
+last_name = 'Noh';
+
+full_name = `${first_name} ${last_name}`;
+
+console.log(full_name);
