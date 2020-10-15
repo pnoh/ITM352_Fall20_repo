5598be3fb87933c0f87a3d6af58ea6433ddea9ce
pnoh <pnoh@hawaii.edu>
2020-10-15 10:31:56 -1000

    a

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,14 +1,8 @@
 age_count = 1; //start the age counter 
 age = 19;
 
-while(age_count < age) {
-    if(age_count >= age/2) {
-        console.log("Don’t ask how old I am!");
-        process.exit();
-    } else {
+while(age_count <= age) {
         console.log(`age ${age_count}`);
-    }
-
-    age_count++;
+        age_count++;
 }
 console.log(`Philip is ${age_count} years old`);
